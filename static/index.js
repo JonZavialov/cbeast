5e5d30d50fe3e7778eb537736b9ffd361d589d32
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-10-04 23:09:17 -0400

    created about us page with rest api and alternating floats

--- static/index.js
+++ static/index.js
@@ -1,14 +1,35 @@
-function setAboutUs(){
+async function setAboutUs(){
     fetch("/data")
     .then(res => res.json())
     .then(async(aboutUsDict) => {
         keys = Object.keys(aboutUsDict)
-        counter = 1
+        counter = 2
+        aboutus = await document.getElementById("aboutus")
+        console.log(aboutus)
         keys.forEach(name => {
+            console.log(`adding ${ name }`)
+            
             image = aboutUsDict[name].img
             position = aboutUsDict[name].position
             bio = aboutUsDict[name].bio
             contact = aboutUsDict[name].contact
+
+            if(counter%2==0) alignment = "left"
+            else alignment = "right"
+
+            aboutus.innerHTML += `
+            <div class=\"${ alignment }\">
+                <img src=\"${ image }\" alt=\"${ name }\" width=200px>
+                <div class=\"${ alignment }-text\"
+                    <p class=\"name\">${ name }</p>
+                    <p class=\"position\">${ position }</p>
+                    <p class=\"contact\">${ contact }</p>
+                    <p class = \"bio\">${ bio }</p>
+                </div>
+            </div>
+            `
+            
+            counter++
         })
     })
 }
